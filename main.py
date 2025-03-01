6202a97fc8d1c2bfdaa32b8c86dc438ce95d73ff
Gildácio Lopes <gildacioj399@gmail.com>
2025-03-01 01:42:40 -0300

    feat: sending emails with the data acquired in the table

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import win32com.client as win32
 
+# Ler a base de dados
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
+# visualizar a base de dados
 pd.set_option('display.max_columns', None)
 print(tabela_vendas)
 
@@ -20,3 +23,32 @@ print('-' * 50)
 # ticket médio por produto em cada loja
 ticket_medio = (faturamento / quantidade).to_frame(name="Ticket Médio")
 print(ticket_medio)
+
+# enviar um email com o relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'contato.gildaciolopes@gmail.com'
+mail.Subject = 'Relatório de Vendas por Loja'
+mail.HTMLBody = f'''
+<p>Prezados,</p>
+
+<p>Segue o Relatório de Vendas por cada Loja.</p>
+
+<p>Faturamento:</p>
+{faturamento_df.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
+
+<p>Quantidade Vendida:</p>
+{quantidade_df.to_html()}
+
+<p>Ticket Médio dos Produtos em cada Loja:</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p>Qualquer dúvida estou à disposição.</p>
+
+<p>Att.,</p>
+<p>Gildácio</p>
+'''
+
+mail.Send()
+
+print('Email Enviado')
